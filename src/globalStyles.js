7ee6ba434b1f1e8aeb30995d7dd6ea0e536f5941
Javier Rodriguez Rodriguez <javier.softdev@gmail.com>
2021-03-05 19:16:08 +0100

    first commit

---
+++ src/globalStyles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle, withTheme } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  *:not(i){
+    font-family: 'Roboto' !important;
+  }
+
+  html, body{
+    -webkit-font-smoothing: antialiased;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background-color: #ffffff;
+    margin: 0 auto;
+    @media screen and (max-width: 768px) {
+      overflow-y: auto;
+      -webkit-overflow-scrolling: touch;
+    }
+  }
+`
+
+export default withTheme(GlobalStyles)
